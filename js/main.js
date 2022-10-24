a26b20ceeb7d44b90668171ab277d728047e5f54
coderrob22 <robertknott7@gmail.com>
2022-10-24 16:27:08 -0400

    stylesheet updates and js set up

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 // Initialize Game Board on Load
 initBoard()
+initCatRow()
+
+function initCatRow(){
+    let catRow = document.getElementById('category-row')
+    
+    for (let k=0; k<6; k++){
+        let box= document.createElement('div')
+        box.className= 'clue-box category-box'
+        catRow.appendChild(box)
+       
+    }
+}
 
 function initBoard(){
     let board = document.getElementById('clue-board')
 
     //Generate 5 rows, then place 6 boxes in each row
-}   for (let i = 0; i < 5; i++){
+   for (let i = 0; i < 5; i++){
         let row = document.createElement('div')
-        let boxValue = 200* (i+1)
+        let boxValue = 200 * (i+1)
         row.className = 'clue-row'
 
         for (let j=0; j<6; j++){
@@ -16,7 +28,12 @@ function initBoard(){
             box.textContent= '$' + boxValue
 
             //box.appendChild(document.createTextNode(boxValue)) Backwards Compatible
-            box.addEventListener('click', getClue, flase)
+            box.addEventListener('click', getClue, false)
             row.appendChild(box)
         }
+        board.appendChild(row)
+}
+}
+function getClue(){
+    console.log('Time for a clue!')
 }
